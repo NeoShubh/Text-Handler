012d33fa2ef6d86bfd22a0266ae65af6ac9498b5
Cap <shubhamrathore1024@gmail.com>
2022-01-12 13:38:38 +0530

    deploy my react app pal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import Contactus from "./components/Contactus";
+//whatever we write here is going to index.js as a component
+//one more thing is that we write something in app.js in bottom
+//write title = 123 then it will send error
+// let  a = "My First React App";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
+  const [mode, setmode] = useState("light");
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setalert(null);
+    }, 2000);
+  };
+
+  const togglemode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#38373A";
+      document.title = "Text-Handler 🌑 Dark Mode";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "#FBEAFF";
+      document.title = "Text-Handler 🌞 Light Mode";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      {/* <h1>{a}</h1> */}
+      <Navbar
+        title="React App"
+        mode={mode}
+        togglemode={togglemode}
+        about="about my app"
+      />
+      <Alert alert={alert} />
+      <div className="container" my-3>
+      <Switch>
+          <Route exact path="/about">
+            <About mode={mode}/>
+          </Route>
+          
+          <Route exact path="/">
+          <TextForm
+          showAlert={showAlert}
+          heading="Enter The Text In The Form Below"
+        />
+          </Route>
+          <Route exact path="/Contactus">
+            <Contactus mode={mode}/>
+          </Route>
+        </Switch>
+        
+
+        
+
+        <Footer mode={mode} />
+      </div>
+      </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')//we redor app on root i guess
 );
 
 // If you want to start measuring performance in your app, pass a function
